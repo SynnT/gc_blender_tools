408475b9872b13b1a6c6189fa5fead7d0b5bdc5a
Gabriel Oliveira <gabriel.oliveira@codejr.com.br>
2020-01-08 21:30:49 -0300

    First steps.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A Blender add-on to import Perfect 3D Model (.p3m) files
 
 ----
-```c
+```cpp
 #define INVALID_BONE_INDEX 255 // or 0xFF
 
 struct vector_t {
@@ -65,113 +65,6 @@ struct p3m_file {
 };
 ```
 
-```
-bool GCDeviceMeshP3M::Load()
-{
-    GCMemoryFile memfile(g_pGCDeviceManager->GetMassFileManager(), m_strFileName);
-
-    if (!memfile.IsLoaded())
-    {
-        m_eDeviceState = GCDS_DISABLE;
-        return false;
-    }
-
-    char strVersion[27];
-    memfile.ReadFile(strVersion, sizeof(char) * 27);
-
-    BYTE NumPositionBone;
-    memfile.ReadFile(&(NumPositionBone), sizeof(BYTE));
-    m_dwNumPositionBone = NumPositionBone;
-
-    BYTE NumAngleBone;
-    memfile.ReadFile(&(NumAngleBone), sizeof(BYTE));
-    m_dwNumAngleBone = NumAngleBone;
-
-    KAngleBoneFromFile *pTempABone = new KAngleBoneFromFile[m_dwNumAngleBone];
-
-    KPositionBone *pPositionBone = new KPositionBone[m_dwNumPositionBone];
-    KAngleBoneOnMemory *pAngleBone = new KAngleBoneOnMemory[m_dwNumAngleBone];
-
-    memfile.ReadFile(pPositionBone, sizeof(KPositionBone) * m_dwNumPositionBone);
-    memfile.ReadFile(pTempABone, sizeof(KAngleBoneFromFile) * m_dwNumAngleBone);
-
-    for (DWORD l = 0; l < m_dwNumAngleBone; l++)
-    {
-        for (int i = 0; i < (int)pAngleBone[l].acChildIndex.size(); ++i)
-        {
-            pAngleBone[l].acChildIndex[i] = pTempABone[l].acChildIndex[i];
-        }
-    }
-
-    SetAngleBon(pAngleBone, m_dwNumAngleBone);
-    SetPositionBone(pPositionBone, m_dwNumPositionBone);
-
-    SAFE_DELETE_ARRAY(pTempABone);
-
-    WORD wTemp;
-    memfile.ReadFile(&wTemp, sizeof(WORD));
-    m_dwNumVertex = wTemp;
-    memfile.ReadFile(&wTemp, sizeof(WORD));
-    m_dwNumFace = wTemp;
-
-    char strTextureFileName[260];
-    memfile.ReadFile(strTextureFileName, sizeof(char) * 260);
-
-    std::vector<ONE_TRIANGLE> vecIndex;
-    vecIndex.reserve(m_dwNumFace);
-    for (DWORD i = 0; i < m_dwNumFace; ++i)
-    {
-        ONE_TRIANGLE triangle;
-        memfile.ReadFile(&triangle, sizeof(ONE_TRIANGLE));
-        vecIndex.push_back(triangle);
-    }
-    WORD *ix;
-
-    std::set<DWORD> setIgIdx;
-    std::vector<SKINVERTEX> vecVertex;
-    SKINVERTEX_SOURCE *v2 = (SKINVERTEX_SOURCE *)memfile.GetDataPointer();
-    for (DWORD i = 0; i < m_dwNumVertex; ++i)
-    {
-        BYTE idx = (BYTE)(v2[i].index - m_dwNumPositionBone);
-        if (m_cIgBoneIdx != -1 && (idx == (BYTE)m_cIgBoneIdx || idx == (BYTE)m_cIgBoneIdx + 1)) // 최후의 하드코딩..
-        {
-            setIgIdx.insert(i);
-        }
-
-        SKINVERTEX v;
-        v.Pos = v2[i].Pos;
-        v.w[0] = 1.0f;
-        v.indexByte[0] = v.indexByte[1] = v.indexByte[2] = v.indexByte[3] = idx;
-        v.Nor = v2[i].Nor;
-        v.tu = v2[i].tu;
-        v.tv = v2[i].tv;
-
-        vecVertex.push_back(v);
-    }
-    
-    vecIndex.erase(std::remove_if(vecIndex.begin(), vecIndex.end(), RemoveFunctor(setIgIdx)), vecIndex.end());
-    m_dwNumFace = (DWORD)vecIndex.size();
-
-    SAFE_RELEASE(m_pIndexBuffer);
-    if (D3D_OK != m_pd3dDevice->CreateIndexBuffer(sizeof(ONE_TRIANGLE) * m_dwNumFace, D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED, &m_pIndexBuffer, NULL))
-        goto Failed;
-    m_pIndexBuffer->Lock(0, 0, (void **)&ix, 0);
-    memcpy(ix, &(*vecIndex.begin()), sizeof(ONE_TRIANGLE) * m_dwNumFace);
-    m_pIndexBuffer->Unlock();
-
-    SKINVERTEX *v1;
-    SAFE_RELEASE(m_pVertexBuffer);
-    if (D3D_OK != m_pd3dDevice->CreateVertexBuffer(m_dwNumVertex * sizeof(SKINVERTEX), D3DUSAGE_WRITEONLY, SKINVERTEX::FVF, D3DPOOL_MANAGED, &m_pVertexBuffer, NULL))
-        goto Failed;
-    m_pVertexBuffer->Lock(0, 0, (void **)&v1, 0);
-    memcpy(v1, &(*vecVertex.begin()), m_dwNumVertex * sizeof(SKINVERTEX));
-    m_pVertexBuffer->Unlock();
-
-    m_eDeviceState = GCDS_LOADED;
-    return true;
-
-Failed:
-    m_eDeviceState = GCDS_DISABLE;
-    return false;
-}
+```cpp
+bool GCDeviceMeshP3M::Load();
 ```
---
+++ p3m_importer.py
@@ -0,0 +1,163 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name": "Perfect 3D Model (.p3m) Importer",
+    "author": "Gabriel F. (Frihet Dev)",
+    "description": "Imports .p3m files into Blender, including meshes and bones.",
+    "blender": (2, 80, 0),
+    "version": (0, 0, 1),
+    "location": "File > Import > Perfect 3D Model (.p3m)",
+    "warning": "",
+    "category": "Import-Export"
+}
+
+
+import bpy
+import os
+import mathutils
+import struct
+
+
+def import_p3m(context, filepath):
+    AUTO_SIZE_SCALE = 0.0045767944 * 1.1
+
+    model_name = bpy.path.basename(filepath)
+
+    print("Importing", model_name)
+
+    model_name = os.path.splitext(model_name)[0]
+
+    file_object = open(filepath, 'rb')
+
+    file_object.read(27) # skips the version
+
+    data = file_object.read(2)
+    bone_position_count, bone_angle_count = struct.unpack('<2B', data)
+
+    # DEBUG
+    print("bone_position_count:", bone_position_count)
+    print("bone_angle_count:", bone_angle_count)
+
+    # TEST
+    armature = bpy.data.armatures.new('Armature')
+    armature_object = bpy.data.objects.new('Armature_object', armature)
+
+    bpy.context.collection.objects.link(armature_object)
+    context.view_layer.objects.active = armature_object
+
+    bpy.ops.object.mode_set(mode='EDIT')
+
+    for x in range(bone_position_count):
+        data = file_object.read(3 * 4)
+        position_x, position_y, position_z = struct.unpack('<3f', data)
+
+        # scales the vectors back
+        position_x = position_x / AUTO_SIZE_SCALE
+        position_y = position_y / AUTO_SIZE_SCALE
+        position_z = position_z / AUTO_SIZE_SCALE
+
+        # DEBUG
+        print("bone_%d:" % (x))
+        print("\tx: %f\ty: %f\tz: %f" % (position_x, position_y, position_z))
+        
+        for _ in range(10):
+            data = file_object.read(1)
+            child_index, = struct.unpack('<1B', data)
+
+            if child_index != 255:
+                # DEBUG
+                print("\t-> bone_%d" % (child_index))      
+
+        file_object.read(2) # ignores the padding
+
+        # TEST
+        joint = armature.edit_bones.new("bone_%d" % x)
+        joint.head = mathutils.Vector((0, 0, 0))
+        joint.tail = mathutils.Vector((position_x, position_y, position_z))
+
+    for x in range(bone_angle_count):
+        file_object.read(4 * 4)
+        
+        for _ in range(10):
+            data = file_object.read(1)
+            child_index, = struct.unpack('<1B', data)
+
+            if child_index != 255 and child_index != x:
+                # DEBUG
+                print("bone_%d -> bone_%d" % (x, child_index))
+
+                # TEST
+                parent = armature.edit_bones[x]
+                child = armature.edit_bones[child_index]
+
+                child.parent = parent
+                child.head = parent.tail                
+                child.tail = child.tail + parent.tail
+
+        file_object.read(2) # ignores the padding
+
+    # fixes orientation
+    armature.transform(mathutils.Matrix([[-1.0, 0.0, 0.0, 0.0],
+                                          [0.0, 0.0, 1.0, 0.0],
+                                          [0.0, 1.0, 0.0, 0.0],
+                                          [0.0, 0.0, 0.0, 1.0]]))
+
+    return {'FINISHED'}
+
+
+# ImportHelper is a helper class, defines filename and
+# invoke() function which calls the file selector.
+from bpy_extras.io_utils import ImportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
+
+
+class ImportSomeData(Operator, ImportHelper):
+    """Import a P3M file"""
+    bl_idname = "import_model.p3m"
+    bl_label = "Import P3M"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".p3m"
+
+    filter_glob: StringProperty(
+        default="*.p3m",
+        options={'HIDDEN'},
+        maxlen=255,  # Max internal buffer length, longer would be clamped.
+    )
+
+    def execute(self, context):
+        return import_p3m(context, self.filepath)
+
+
+# Only needed if you want to add into a dynamic menu
+def menu_func_import(self, context):
+    self.layout.operator(ImportSomeData.bl_idname, text="Perfect 3D Model (.p3m)")
+
+
+def register():
+    bpy.utils.register_class(ImportSomeData)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImportSomeData)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.import_model.p3m('INVOKE_DEFAULT')
